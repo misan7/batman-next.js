4a5a3e93473e55bfb5b78c9919cef86309a75002
dev <dev@MacBook-Pro-de-dev.local>
2018-03-27 17:55:08 +0200

    post.js Fetch Data in Client Side

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,44 @@
-export default () => <div>This is the about page.</div>;
+import Layout from "../components/MyLayout.js";
+import Link from "next/link";
+
+const About = props => (
+  <Layout>
+    <h1>About Batman TV Shows</h1>
+    <p>
+      Qui deserunt incidunt corporis sapiente possimus beatae et. Perspiciatis
+      quia eius. Ipsam ullam dolore officia. Quae ex numquam ipsa vel qui
+      impedit et. Incidunt velit ea nemo nobis iusto asperiores autem ipsum.
+      Nisi molestiae error labore cupiditate. Iste aut quisquam tempore. Animi
+      fugit voluptatem explicabo velit. Enim dicta ut hic ipsum tempora. Iusto
+      soluta ex et.
+    </p>
+    <p>
+      Totam possimus deserunt autem et sed quia esse. Nostrum aspernatur quas
+      sequi quod est velit saepe modi. Quia vel sapiente eum voluptatem dolorem
+      minima quia eum. Quidem voluptas rem at eaque eius quia incidunt error
+      eum. Praesentium culpa doloribus dolor amet voluptas rerum et. Dolores
+      illum sit fuga enim dicta enim et rerum. Ipsum eveniet occaecati iusto
+      dolore eius voluptates. Explicabo ea cum dolorem nostrum occaecati officia
+      eligendi perferendis. Voluptatem quo sit sed minus. Aliquid ipsum est ut
+      quis illo qui corporis ratione enim. Mollitia impedit iure id nihil
+      consequatur. Totam ut tempora nobis aliquam possimus rerum quos quibusdam
+      sint.
+    </p>
+    <p>
+      Ut et optio eaque distinctio nihil voluptatibus repudiandae explicabo
+      fugit. Aspernatur magnam quos sed alias numquam. Dolorem sed ratione ut
+      consequatur blanditiis voluptas laboriosam quibusdam ut. Nihil distinctio
+      delectus ea voluptatem odit harum in possimus. Corrupti hic nobis nihil
+      non corporis. Omnis autem quia omnis quae omnis at quos. Eligendi
+      voluptatem voluptatem perferendis quaerat iure dolorem consequatur
+      quisquam quae. Saepe excepturi voluptas qui incidunt sunt consequatur
+      corrupti. Ratione nihil dolore quod optio mollitia. Qui aut fuga dicta
+      odio quia dolorem natus. Aut velit at et omnis. Velit non aut. Illo veniam
+      cupiditate. Voluptates magnam autem inventore velit. Non hic rerum esse
+      nemo. Animi repudiandae debitis quidem quo cumque enim eaque aliquam
+      cupiditate.
+    </p>
+  </Layout>
+);
+
+export default About;
